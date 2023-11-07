b511d527642a53a3715a3727586eb3da9af149f4
Emil <emilskovmand@gmail.com>
2023-11-07 11:39:03 +0100

    register

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -6,26 +6,29 @@ const Stack = createNativeStackNavigator()
 
 // Screens
 import { useFonts } from "expo-font"
+import { AuthProvider } from "./hooks/use-auth"
 import SignupScreen from "./routes/SignupScreen"
 import WelcomeScreen from "./routes/WelcomeScreen"
 
 export default function App() {
-    let [fontsLoaded] = useFonts({
-        "Nunito-Black": require("./assets/fonts/NunitoSans-Black.ttf"),
-        "Nunito-Bold": require("./assets/fonts/NunitoSans-Bold.ttf"),
-        "Nunito-ExtraBold": require("./assets/fonts/NunitoSans-ExtraBold.ttf"),
-        "Nunito-ExtraLight": require("./assets/fonts/NunitoSans-ExtraLight.ttf"),
-        "Nunito-Light": require("./assets/fonts/NunitoSans-Light.ttf"),
-        "Nunito-Regular": require("./assets/fonts/NunitoSans-Regular.ttf"),
-        "Nunito-SemiBold": require("./assets/fonts/NunitoSans-SemiBold.ttf"),
-    })
+  let [fontsLoaded] = useFonts({
+    "Nunito-Black": require("./assets/fonts/NunitoSans-Black.ttf"),
+    "Nunito-Bold": require("./assets/fonts/NunitoSans-Bold.ttf"),
+    "Nunito-ExtraBold": require("./assets/fonts/NunitoSans-ExtraBold.ttf"),
+    "Nunito-ExtraLight": require("./assets/fonts/NunitoSans-ExtraLight.ttf"),
+    "Nunito-Light": require("./assets/fonts/NunitoSans-Light.ttf"),
+    "Nunito-Regular": require("./assets/fonts/NunitoSans-Regular.ttf"),
+    "Nunito-SemiBold": require("./assets/fonts/NunitoSans-SemiBold.ttf"),
+  })
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-                <Stack.Screen name="SignupScreen" component={SignupScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} />
+        </Stack.Navigator>
+      </AuthProvider>
+    </NavigationContainer>
+  )
 }
--- frontend/routes/SignupScreen.tsx
+++ frontend/routes/SignupScreen.tsx
@@ -1,38 +1,114 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { StyleSheet, Text, TextInput, View } from "react-native"
 
 // Components
 import { Button } from "../components/index"
+import useAPI from "../hooks/use-api"
+import { useAuth } from "../hooks/use-auth"
 
 export default function SignupScreen() {
-    return (
-        <View style={styles.WelcomeOuter}>
-            <View style={styles.WelcomeLogo}>
-                <Text>Diller</Text>
+  const { request, loading, error } = useAPI({ url: "/user/register" }, { autoGet: false })
+
+  const { authenticate, refresh, user } = useAuth()
+
+  const [registerSuccess, setRegisterSuccess] = useState(false)
+
+  const [form, setForm] = useState({
+    email: "",
+    username: "",
+    password: "",
+    repeatPassword: ""
+  })
+
+  const handleOnRegister = async () => {
+    if (form.password !== form.repeatPassword) {
+      return
+    }
+    const register = await request({ method: "post", body: form })
+    const auth = await authenticate(form.email || "", form.password || "")
+
+    if (!register.error && !auth.error) {
+      setRegisterSuccess(true)
+    }
+  }
+
+
+  return (
+    <View style={styles.welcomeOuter}>
+      <View style={styles.welcomeLogo}>
+        <View style={styles.loginContainer}>
+          <View style={styles.loginContainer}>
+
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputLabel}>Email</Text>
+              <TextInput style={styles.textInput} textContentType="emailAddress" onChangeText={v => setForm({ ...form, email: v })} />
             </View>
-            <View style={styles.WelcomeButtonContainer}>
-                <Button full={true} title={"Register"} />
+
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputLabel}>Username</Text>
+              <TextInput style={styles.textInput} textContentType="username" onChangeText={v => setForm({ ...form, username: v })} />
             </View>
+
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputLabel}>Password</Text>
+              <TextInput secureTextEntry textContentType="password" style={styles.textInput} onChangeText={v => setForm({ ...form, password: v })} />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputLabel}>Repeat Password</Text>
+              <TextInput secureTextEntry textContentType="password" style={styles.textInput} onChangeText={v => setForm({ ...form, repeatPassword: v })} />
+            </View>
+
+          </View>
+
+          <View style={styles.registerButtonContainer}>
+            <Button full={true} onPress={handleOnRegister} title={"Register"} />
+          </View>
         </View>
-    )
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    WelcomeOuter: {
-        backgroundColor: "#000",
-        flex: 1,
-        paddingRight: 15,
-        paddingLeft: 15,
-    },
-    WelcomeLogo: {
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-    },
-    WelcomeButtonContainer: {
-        width: "100%",
-        flex: 1,
-        justifyContent: "flex-end",
-        marginBottom: 46,
-    },
+  welcomeOuter: {
+    backgroundColor: "#000",
+    flex: 1,
+    paddingRight: 15,
+    paddingLeft: 15,
+  },
+  welcomeLogo: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  registerButtonContainer: {
+    width: "100%",
+    marginTop: 40
+  },
+  loginContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+    gap: 8
+  },
+  inputContainer: {
+    width: "100%"
+  },
+  inputLabel: {
+    color: "white",
+    width: "100%",
+  },
+  textInput: {
+    backgroundColor: "#9b9b9b3d",
+    width: "100%",
+    paddingTop: 8,
+    color: "white",
+    fontWeight: "700",
+    paddingBottom: 8,
+    paddingRight: 16,
+    paddingLeft: 16,
+    borderBottomColor: "#9b9b9b",
+    borderWidth: 3,
+  }
 })
